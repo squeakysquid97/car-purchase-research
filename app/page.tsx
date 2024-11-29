e235d3e8d2264f45b495cedc27710839514af02e
Peter Franklin <peter@kgfranklinjr.com>
2024-11-29 17:27:25 -0500

    Componentized the landing page and updated ico/title

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import Image from "next/image";
+import LandingPage from "./landingPage";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={200}
-          height={38}
-          priority
-        />
-        <div className=" ">
-          <h1> Coming Soon! </h1>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
-    </div>
-  );
+  return <LandingPage />;
 }
