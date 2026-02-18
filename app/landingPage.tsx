4224efb810735f8d0b7a33e2aebc6a4bdb0dda34
squeakysquid97 <peter@kgfranklinjr.com>
2026-02-18 08:33:37 -0500

    changed landing page to black background

--- app/landingPage.tsx
+++ app/landingPage.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 export default function LandingPage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
+    <div className="min-h-screen bg-black text-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-0 row-start-2 items-center sm:items-center">
         <Image
           className="dark:invert"
           src="/cprlogo.svg"
           alt="CPR logo"
-          width={200}
-          height={38}
+          width={1000}
+          height={100}
           priority
         />
-        <div className=" ">
+        <div className="text-4xl font-bold">
           <h1> Coming Soon! </h1>
         </div>
       </main>
