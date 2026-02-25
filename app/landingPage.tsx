4046f710f4e06e3087ae7c1ed37d6fdf3114b9ef
squeakysquid97 <peter@kgfranklinjr.com>
2026-02-25 07:28:06 -0500

    changed logo type to webp

--- app/landingPage.tsx
+++ app/landingPage.tsx
@@ -5,7 +5,7 @@ export default function LandingPage() {
     <div className="min-h-screen bg-black text-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-0 row-start-2 items-center sm:items-center">
         <Image
-          src="/cprlogo.svg"
+          src="/cprlogo.webp"
           alt="CPR logo"
           width={1000}
           height={100}
